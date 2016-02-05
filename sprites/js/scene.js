74d15c0ae9d54a46c582a6b845b42ae5e6fca4e6
Trixie Roque <troque@lion.lmu.edu>
2016-02-04 23:15:58 -0800

    Almost done

--- sprites/js/scene.js
+++ sprites/js/scene.js
@@ -3,7 +3,7 @@ $(function () {
 	var ctx = $("#canvas")[0].getContext("2d");
     var drawScene = function () {
     	ctx.save();
-    	ctx.translate(200, 100);
+    	ctx.translate(100, 100);
     	Sprites.bb8({
     		ctx: ctx,
     		x: 200,
@@ -13,15 +13,20 @@ $(function () {
     	ctx.restore();
 
     	ctx.save();
-    	ctx.translate(300, 0);
+    	ctx.translate(700, 50);
+    	ctx.scale(0.5, 0.5);
     	Sprites.kyloRen({
     		ctx: ctx,
     		x: 300,
-    		y: 0
+    		y: 0,
+    		leftArmAngle: Math.PI / 4,
+    		rightArmAngle: Math.PI / 2
     	});
     	ctx.restore();
 
     	ctx.save();
+    	ctx.translate(600, 150);
+    	ctx.scale(0.25, 0.25);
     	Sprites.lightsaber({
     		ctx: ctx,
     		x: 500,
