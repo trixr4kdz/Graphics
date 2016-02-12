38c55caacfe1c744b6985d5132e2738a1c23feb4
Trixie Roque <troque@lion.lmu.edu>
2016-02-11 23:31:53 -0800

    Fixing up

--- sprites/js/bb-8.js
+++ sprites/js/bb-8.js
@@ -10,10 +10,12 @@ $(function () {
 
         var ctx = specs.ctx;
         var headTurn = specs.headTurn || 0;
-        // var bodyTurn = specs.bodyTurn || 0;
+        var bodyTurn = specs.bodyTurn || 0;
         var color = specs.color || "rgb(255, 120, 0)";
         var xBody = specs.x;
         var yBody = specs.y;
+        var xHead = xBody;
+        var yHead = yBody - HEAD_OFFSET;
         var bodyGradient = ctx.createRadialGradient(500, 250, 40, 250, 250, 400);
         var headGradient = ctx.createRadialGradient(500, 200, 60, 300, 100, 450);
 
@@ -37,6 +39,19 @@ $(function () {
             ctx.fill();
             ctx.closePath();
 
+            ctx.save();
+            ctx.beginPath();
+            ctx.translate(xBody, yBody);
+            // ctx.rotate(Math.PI / 180 * 45);
+            ctx.moveTo(-BODY_RADIUS * 0.8, 0);
+            ctx.lineTo(BODY_RADIUS * 0.8, 0);
+            ctx.stroke();
+            ctx.moveTo(0, -BODY_RADIUS * 0.8);
+            ctx.lineTo(0, BODY_RADIUS * 0.8);
+            ctx.stroke();
+            drawBody();
+            ctx.restore();
+            
             ctx.fillStyle = headGradient;
             ctx.beginPath();
             ctx.save();
@@ -46,15 +61,30 @@ $(function () {
             ctx.fill();
             ctx.restore();
 
+            ctx.save();
+            ctx.translate(xHead, yHead);
             drawEye(headTurn);
-            drawBody();
+            ctx.restore();
+        }
+
+        var rotateBody = function (turn) {
+            // Only needs to go from 0 to 90
         }
 
         var drawEye = function (turn) {
+            var RIGHTEST = 40;
+            var LEFTEST = -40;
             var EYE_OFFSET = 45;
             var LOWER_DOT_OFFSET = 25;
-            drawInnerCircles(ctx, "black", 15, xBody + turn, yBody - HEAD_OFFSET - EYE_OFFSET);
-            drawInnerCircles(ctx, "black", 5, xBody + LOWER_DOT_OFFSET + turn, yBody - HEAD_OFFSET - LOWER_DOT_OFFSET);
+            var SHINY_OFFSET = 5;
+            if (turn < LEFTEST || turn > RIGHTEST) {
+                turn = turn < LEFTEST ? LEFTEST : RIGHTEST;
+            }
+            drawInnerCircles(ctx, "black", 15, turn, - EYE_OFFSET);
+            drawInnerCircles(ctx, "white", 5, turn - SHINY_OFFSET, -EYE_OFFSET - SHINY_OFFSET);
+            if (turn < RIGHTEST - 10) {
+                drawInnerCircles(ctx, "black", 5, LOWER_DOT_OFFSET + turn, -LOWER_DOT_OFFSET);
+            }
         }
 
         var drawInnerCircles = function (ctx, fillStyle, radius, x, y) {
@@ -73,21 +103,24 @@ $(function () {
         }
 
         var drawOuterCircles = function (ctx, s0, s1, s2, s3, c0, c1, c2, c3) {
+            ctx.save();
+            // ctx.translate(xBody, yBody);
             ctx.beginPath()
-            ctx.moveTo(xBody + (s0 * BODY_RADIUS * c0), yBody + (s2 * BODY_RADIUS * c1));
-            ctx.lineTo(xBody + (s1 * BODY_RADIUS * c0), yBody + (s3 * BODY_RADIUS * c1));
-            ctx.lineTo(xBody + (s1 * BODY_RADIUS * c0) + (s0 * c2), yBody + (s3 * BODY_RADIUS * c1) + (s2 * c3));
-            ctx.lineTo(xBody + (s0 * BODY_RADIUS * c0) + (s1 * c2), yBody + (s2 * BODY_RADIUS * c1) + (s3 * c3));
+            ctx.moveTo( (s0 * BODY_RADIUS * c0),(s2 * BODY_RADIUS * c1));
+            ctx.lineTo((s1 * BODY_RADIUS * c0),(s3 * BODY_RADIUS * c1));
+            ctx.lineTo((s1 * BODY_RADIUS * c0) + (s0 * c2),(s3 * BODY_RADIUS * c1) + (s2 * c3));
+            ctx.lineTo((s0 * BODY_RADIUS * c0) + (s1 * c2),(s2 * BODY_RADIUS * c1) + (s3 * c3));
             ctx.closePath();
             ctx.fill();
             ctx.stroke();
+            ctx.restore();
         }
 
         var drawBody = function () {
             drawCenterCross(ctx, xBody, yBody + (CENTER_CIRCLE_RADIUS), xBody, yBody - (CENTER_CIRCLE_RADIUS))
             drawCenterCross(ctx, xBody - CENTER_CIRCLE_RADIUS, yBody, xBody + CENTER_CIRCLE_RADIUS, yBody);
-            drawInnerCircles(ctx, color, HEAD_RADIUS * .80, xBody, yBody);
-            drawInnerCircles(ctx, "white", HEAD_RADIUS / 1.75, xBody, yBody);
+            drawInnerCircles(ctx, color, HEAD_RADIUS * .80, 0, 0);
+            drawInnerCircles(ctx, "white", HEAD_RADIUS / 1.75, 0, 0);
 
             ctx.fillStyle = color;
 
