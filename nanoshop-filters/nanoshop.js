42eea2bcf2c14157b6535cab087e602e59eecda4
Trixie Roque <troque@lion.lmu.edu>
2016-02-18 11:54:48 -0800

    Copied bazaar files to start with this assignment

---
+++ nanoshop-filters/nanoshop.js
@@ -0,0 +1,32 @@
+/*
+ * This is a very simple module that demonstrates rudimentary,
+ * pixel-level image processing.
+ */
+var Nanoshop = {
+    /*
+     * Applies the given filter to the given ImageData object,
+     * then modifies its pixels according to the given filter.
+     *
+     * A filter is a function (x, y, r, g, b, a) that returns another
+     * pixel as a 4-element array representing an RGBA value.
+     */
+    applyFilter: function (imageData, filter) {
+        // For every pixel, replace with something determined by the filter.
+        var pixelArray = imageData.data;
+
+        for (var i = 0, max = imageData.width * imageData.height * 4; i < max; i += 4) {
+            var pixelIndex = i / 4;
+
+            var pixel = filter(
+                pixelIndex % imageData.width, Math.floor(pixelIndex / imageData.height),
+                pixelArray[i], pixelArray[i + 1], pixelArray[i + 2], pixelArray[i + 3]
+            );
+
+            for (var j = 0; j < 4; j += 1) {
+                pixelArray[i + j] = pixel[j];
+            }
+        }
+
+        return imageData;
+    }
+};
