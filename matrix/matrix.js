1f4aa62396b01a28b57b06fe7112028dc0fa42e0
Trixie Roque <troque@lion.lmu.edu>
2016-03-29 01:34:25 -0700

    Pass default test

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -11,10 +11,99 @@
  * vectors.  Specific Vector2D and Vector3D implementations can be much
  * more compact, while sacrificing generality.
  */
+var Matrix = (function () {
+    var matrix = function() {
+        this.elements = [
+            1, 0, 0, 0, 
+            0, 1, 0, 0, 
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ]
+    };
+
+    var getRotationMatrix = function (angle, x, y, z) {
+        // In production code, this function should be associated
+        // with a matrix object with associated functions.
+        var axisLength = Math.sqrt((x * x) + (y * y) + (z * z));
+        var s = Math.sin(angle * Math.PI / 180.0);
+        var c = Math.cos(angle * Math.PI / 180.0);
+        var oneMinusC = 1.0 - c;
+
+        // Normalize the axis vector of rotation.
+        x /= axisLength;
+        y /= axisLength;
+        z /= axisLength;
+
+        // Now we can calculate the other terms.
+        // "2" for "squared."
+        var x2 = x * x;
+        var y2 = y * y;
+        var z2 = z * z;
+        var xy = x * y;
+        var yz = y * z;
+        var xz = x * z;
+        var xs = x * s;
+        var ys = y * s;
+        var zs = z * s;
+
+        // GL expects its matrices in column major order.
+        return [
+            (x2 * oneMinusC) + c,
+            (xy * oneMinusC) + zs,
+            (xz * oneMinusC) - ys,
+            0.0,
+
+            (xy * oneMinusC) - zs,
+            (y2 * oneMinusC) + c,
+            (yz * oneMinusC) + xs,
+            0.0,
+
+            (xz * oneMinusC) + ys,
+            (yz * oneMinusC) - xs,
+            (z2 * oneMinusC) + c,
+            0.0,
+
+            0.0,
+            0.0,
+            0.0,
+            1.0
+        ];
+    };
+
+    var getOrthoMatrix = function (left, right, bottom, top, zNear, zFar) {
+        var width = right - left;
+        var height = top - bottom;
+        var depth = zFar - zNear;
+
+        return [
+            2.0 / width,
+            0.0,
+            0.0,
+            0.0,
+
+            0.0,
+            2.0 / height,
+            0.0,
+            0.0,
+
+            0.0,
+            0.0,
+            -2.0 / depth,
+            0.0,
+
+            -(right + left) / width,
+            -(top + bottom) / height,
+            -(zFar + zNear) / depth,
+            1.0
+        ];
+    };
+    return matrix;
+})();
+
 var Vector = (function () {
     // Define the constructor.
     var vector = function () {
-        this.elements = [].slice.call(arguments);
+        this.encapsulatements = [].slice.call(arguments);
     };
     
     // A private method for checking dimensions,
--- matrix/tests/test-matrix.js
+++ matrix/tests/test-matrix.js
@@ -1,7 +1,43 @@
 var	expect = chai.expect;
 
-describe("Initialize unit testing.", function () {
-	it("Does nothing", function (done) {
+describe("Constructing a matrix", function () {
+	it("successfully creates the default 4x4 matrix", function (done) {
+		var m = new Matrix();
+		expect(m.elements).to.eql([
+			1, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1
+			]);
+		var v = new Vector();
+
 		done();
-	})
-})
+	});
+});
+
+describe("Matrix multiplication", function () {
+	it("successfully multiplies 2 matrices", function (done) {
+		done();
+	});
+});
+
+
+describe("Rotating", function () {
+	it("successfully rotates the matrix", function (done) {
+		done();
+	});
+});
+
+describe("Translating", function () {
+	it("successfully translates the matrix", function (done) {
+		done();
+	});
+});
+
+describe("Scaling", function () {
+	it("successfully scales the matrix", function (done) {
+		done();
+	});
+});
+
+
