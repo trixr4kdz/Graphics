dbc4776de2496893681c4e98a547d47a42f632bc
Trixie Roque <troque@lion.lmu.edu>
2016-03-29 00:07:20 -0700

    Made tests look cleaner

--- matrix/tests/test-matrix.js
+++ matrix/tests/test-matrix.js
@@ -1,3 +1,7 @@
-$(function () {
-    
+var	expect = chai.expect;
+
+describe("Initialize unit testing.", function () {
+	it("Does nothing", function (done) {
+		done();
+	})
 })
--- pipeline/tests/test-shapes.js
+++ pipeline/tests/test-shapes.js
@@ -1,4 +1,4 @@
-var should = require('should');
+var	expect = chai.expect;
 
 describe("Initialize unit testing.", function () {
 	it("Does nothing", function (done) {
